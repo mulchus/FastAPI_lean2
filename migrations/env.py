1bde71ece4dd0a2658283a76d48fe7840a51e583
mulchus <mulch@list.ru>
2024-07-21 23:31:56 +0300

    update sys path on src

--- migrations/env.py
+++ migrations/env.py
@@ -4,8 +4,14 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from config import DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME
-from models.models import metadata
+
+import os
+import sys
+sys.path.append(os.path.join(sys.path[0], 'src'))
+
+from src.config import DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME
+from src.auth.models import metadata as auth_metadata
+from src.operations.models import metadata as operations_metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -29,7 +35,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = [auth_metadata, operations_metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
